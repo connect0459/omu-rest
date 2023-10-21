10748f402138a9b5abded4ac79bf77b2b56114c7
connect0459 <connect0459@gmail.com>
2023-10-21 19:35:29 +0900

    Swagger.phpを更新

--- app/Http/Controllers/Swagger.php
+++ app/Http/Controllers/Swagger.php
@@ -16,8 +16,8 @@ use OpenApi\Annotations as OA;
  *
  * サーバー情報
  * @OA\Server(
- *   description="lilipopサーバーにデプロイしたAPIエンドポイント",
- *   url="https://rest.studyfortwo.org/public/api"
+ *   description="ローカルホストのAPIエンドポイント",
+ *   url="http://localhost:8000/api"
  * )
  * 
  * セキュリティスキーマ
